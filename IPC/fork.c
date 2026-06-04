04853f3c01e04c61d6a941f440d3dd3a6858ff1d
Bhavesh12z <guptabhavi06@gmail.com>
2026-06-04 05:42:23 +0000

    fork basic 2

--- IPC/fork.c
+++ IPC/fork.c
@@ -5,6 +5,14 @@
 
 int main(int argc,char* argv[]){
 	int id = fork();
-	printf(" Hi from id  : %d \n ",id);
+	printf("Hello  i'm id : %d \n ",id);
+	if(id == 0){
+		printf(" hehe child process is here \n ");
+	}
+	else{
+		printf("parent process is here and my child's id is %d \n",id);
+	}
+	
+	printf("Done process by id : %d \n ",id);
 	return 0;
 }
